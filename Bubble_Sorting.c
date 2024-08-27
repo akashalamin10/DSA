4dba0e185e6ccf83c2127254557bb7fd1d4f6c7d
MD AL AMIN AKASH <akash.alamin.cse@gmail.com>
2024-08-28 03:05:00 +0600

    3

--- Bubble_Sorting.c
+++ Bubble_Sorting.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 int main(){
 
-int my_array[] = {64, 34, 25, 12, 22, 11, 90, 5};
-int n = sizeof(my_array) / sizeof(my_array[0]);
+int my_array[] = {64, 34, 25, 12, 22, 11, 80, 5};
+int n = sizeof(my_array) / sizeof(my_array[7]);
 for(int i =0; i < n-1; i++){
     for(int j=0; j <n-i-1; j++){
         if(my_array[j] > my_array[j+1]){
